7c9ce0ee537002a6648132e54231710db9dacc94
scuddi <157764402+scuddi@users.noreply.github.com>
2024-04-18 20:06:23 +0200

    imported more buttons and created the input frame, CTk Window not working yet due to some mistakes in code

--- taschenrechner_V04_CTK.py
+++ taschenrechner_V04_CTK.py
@@ -79,4 +79,137 @@ window.geometry("430x350")
 
 window.title("Wilkommen in meinem Taschenrechner!")
 
+# input field
+
+input_field = customtkinter.CTkEntry(master = window,
+                                     width = 410,
+                                     justify = "center",
+                                     font = ("Arial", 20))
+
+#output field
+
+output_frame = customtkinter.CTkFrame(master = window,
+                                      width = 410,
+                                      height = 60,
+                                      fg_color = "#383838")
+
+output_field = customtkinter.CTkLabel(output_frame,
+                                      #text = output,
+                                      width = 390
+                                      )
+
+#operator buttons
+
+equal_button = customtkinter.CTkButton(master = window,
+                                       text = "=",
+                                       width = "8",
+                                       height = "3",
+                                       #command = equal
+                                       )
+
+plus_button = customtkinter.CTkButton(master = window,
+                                      text = "+",
+                                      width = "8",
+                                      height = "3",
+                                      #command = plus
+                                      )
+
+minus_button = customtkinter.CTkButton(master = window,
+                                       text= "-",
+                                       width = "8",
+                                       height = "3",
+                                       #command = minus
+                                       )
+
+multiplicator_button = customtkinter.CTkButton(master = window,
+                                               text="*",
+                                               width = "8",
+                                               height = "3",
+                                               #command = multiplicator
+                                               )
+
+division_button = customtkinter.CTkButton(master = window,
+                                          text = "/",
+                                          width = "8",
+                                          height = "3",
+                                          #command = division
+                                          )
+
+rest_button = customtkinter.CTkButton(master = window,
+                                      text = "Rest,",
+                                      width = "8",
+                                      height = "3",
+                                      #command= rest
+                                      )
+
+full_delete_button = customtkinter.CTkButton(master = window,
+                                             text = "C",
+                                             width = "8",
+                                             height = "3",
+                                             #command = full_delete
+                                             )
+
+potentiate_button = customtkinter.CTkButton(master = window,
+                                            text = "^",
+                                            width = "8",
+                                            height = "3",
+                                            #command = potentiate
+                                            )
+
+open_bracket_button = customtkinter.CTkButton(master = window,
+                                              text="(",
+                                              width = "8",
+                                              height = "3",
+                                              #command = open_bracket
+                                              )
+
+closing_bracket_button = customtkinter.CTkButton(master = window,
+                                                 text=")",
+                                                 width = "8",
+                                                 height = "3",
+                                                 #command = close_bracket
+                                                 )
+
+del_button = customtkinter.CTkButton(master = window,
+                                     text="Del",
+                                     width = "8",
+                                     height = "3",
+                                     #command = partial_delete
+                                     )
+
+#define grid
+
+window.columnconfigure((0,1,2,3), weight = 0)
+window.rowconfigure((0,1,2,3), weight = 0)
+
+#input field
+
+input_field.grid(row = 0, column = 0, padx = 10, pady = 10)
+output_frame.grid(row = 1, column = 0, padx = 10, pady = 5)
+output_field.grid(row=1, column = 0, padx = 10, pady = 5)
+
+# buttons
+"""
+number_7.grid(row=3, column=0)
+number_8.grid(row=3, column=1)
+number_9.grid(row=3, column=2)
+plus_button.grid(row=3, column=3)
+number_4.grid(row=4, column=0)
+number_5.grid(row=4, column=1)
+number_6.grid(row=4, column=2)
+minus_button.grid(row=4, column=3)
+number_1.grid(row=5, column=0)
+number_2.grid(row=5, column=1)
+number_3.grid(row=5, column=2)
+multiplicator_button.grid(row=5, column=3)
+number_0.grid(row=6, column=0)
+open_bracket_button.grid(row=6, column=1)
+closing_bracket_button.grid(row=6, column=2)
+division_button.grid(row=6, column=3)
+rest_button.grid(row=7, column=0)
+del_button.grid(row=7, column=1)
+full_delete_button.grid(row=7, column=2)
+equal_button.grid(row=7, column=3)
+"""
+
 window.mainloop()
