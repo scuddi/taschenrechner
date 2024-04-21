0606e483112961c9492ebf1149ceb92f8b66f0ef
scuddi <157764402+scuddi@users.noreply.github.com>
2024-04-21 15:35:00 +0200

    finished the GUI, changed some colours, added back the input and output fields as well as all functions;
    this version of the calculator is finished
    
    Possible todos for the future:
    - more functions
    - written with OOP
    - dynamic resizing of the window

--- taschenrechner_V04_CTK.py
+++ taschenrechner_V04_CTK.py
@@ -2,182 +2,196 @@ import customtkinter
 
 #functions for the buttons of the
 
-output = "Test"
+output = ""
 
-"""
+
 def plus():
-    input_field.insert(CTk.END,"+")
+    input_field.insert(customtkinter.END,"+")
 
 def minus():
-    input_field.insert(CTk.END,"-")
+    input_field.insert(customtkinter.END,"-")
 
 def multiplicator():
-    input_field.insert(CTk.END,"*")
+    input_field.insert(customtkinter.END,"*")
 
 def potentiate():
-    input_field.insert(CTk.END, "**")
+    input_field.insert(customtkinter.END, "**")
 
 def open_bracket():
-    input_field.insert(CTk.END,"(")
+    input_field.insert(customtkinter.END,"(")
 
 def close_bracket():
-    input_field.insert(CTk.END, ")")
+    input_field.insert(customtkinter.END, ")")
 
 def division():
-    input_field.insert(CTk.END,"/")
+    input_field.insert(customtkinter.END,"/")
 
 def rest():
-    input_field.insert(CTk.END,"%")
+    input_field.insert(customtkinter.END,"%")
 
 def full_delete():
-    input_field.delete(0, CTk.END)
-    output_field.config(text="")
+    input_field.delete(0, customtkinter.END)
+    output_field.configure(text="")
 
 def partial_delete():
     input_field.delete(input_field.index("end") - 1)
 
 def n_0():
-    input_field.insert(CTk.END, 0)
+    input_field.insert(customtkinter.END, 0)
 def n_1():
-    input_field.insert(CTk.END, 1)
+    input_field.insert(customtkinter.END, 1)
 
 def n_2():
-    input_field.insert(CTk.END, 2)
+    input_field.insert(customtkinter.END, 2)
 
 def n_3():
-    input_field.insert(CTk.END, 3)
+    input_field.insert(customtkinter.END, 3)
 
 def n_4():
-    input_field.insert(CTk.END, 4)
+    input_field.insert(customtkinter.END, 4)
 
 def n_5():
-    input_field.insert(CTk.END, 5)
+    input_field.insert(customtkinter.END, 5)
 
 def n_6():
-    input_field.insert(CTk.END, 6)
+    input_field.insert(customtkinter.END, 6)
 
 def n_7():
-    input_field.insert(CTk.END, 7)
+    input_field.insert(customtkinter.END, 7)
 
 def n_8():
-    input_field.insert(CTk.END, 8)
+    input_field.insert(customtkinter.END, 8)
 
 def n_9():
-    input_field.insert(CTk.END, 9)
+    input_field.insert(customtkinter.END, 9)
 
-output = ""
 def equal():
     middle = input_field.get()
     output = eval(middle)
-    output_field.config(text=output)
+    output_field.configure(text=output)
 
-"""
+
 # window setup
 
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 
 window = customtkinter.CTk()
-window.geometry("430x350")
+window.geometry("420x380")
 
 window.title("Wilkommen in meinem Taschenrechner!")
 
 # input field
-"""
+
 input_field = customtkinter.CTkEntry(master = window,
-                                     width = 410,
+                                     width = 390,
+                                     height = 40,
                                      justify = "center",
                                      font = ("Arial", 20))
-"""
+
 #output field
-"""
+
 output_frame = customtkinter.CTkFrame(master = window,
-                                      width = 410,
-                                      height = 60,
+                                      width = 390,
+                                      height = 50,
                                       fg_color = "#383838")
+
 
 output_field = customtkinter.CTkLabel(output_frame,
                                       text = output,
-                                      width = 390
+                                      width = 370,
+                                      height = 40,
+                                      font = ("Arial", 20)
                                       )
-"""
+
 #operator buttons
 
 equal_button = customtkinter.CTkButton(master = window,
                                        text = "=",
                                        width = 100,
                                        height = 50,
-                                       #command = equal
+                                       fg_color = "#545454",
+                                       command = equal
                                        )
 
 plus_button = customtkinter.CTkButton(master = window,
                                       text = "+",
                                       width = 100,
                                       height = 50,
-                                      #command = plus
+                                      fg_color = "#545454",
+                                      command = plus
                                       )
 
 minus_button = customtkinter.CTkButton(master = window,
                                        text= "-",
                                        width = 100,
                                        height = 50,
-                                       #command = minus
+                                       fg_color = "#545454",
+                                       command = minus
                                        )
 
 multiplicator_button = customtkinter.CTkButton(master = window,
                                                text="*",
                                                width = 100,
                                                height = 50,
-                                               #command = multiplicator
+                                               fg_color = "#545454",
+                                               command = multiplicator
                                                )
 
 division_button = customtkinter.CTkButton(master = window,
                                           text = "/",
                                           width = 100,
                                           height = 50,
-                                          #command = division
+                                          fg_color = "#545454",
+                                          command = division
                                           )
 
 rest_button = customtkinter.CTkButton(master = window,
                                       text = "Rest",
                                       width = 100,
                                       height = 50,
-                                      #command= rest
+                                      fg_color = "#545454",
+                                      command= rest
                                       )
 
 full_delete_button = customtkinter.CTkButton(master = window,
                                              text = "C",
                                              width = 100,
                                              height = 50,
-                                             #command = full_delete
+                                             fg_color = "#545454",
+                                             command = full_delete
                                              )
 
 potentiate_button = customtkinter.CTkButton(master = window,
                                             text = "^",
                                             width = 100,
                                             height = 50,
-                                            #command = potentiate
+                                            fg_color = "#545454",
+                                            command = potentiate
                                             )
 
 open_bracket_button = customtkinter.CTkButton(master = window,
                                               text="(",
                                               width = 100,
                                               height = 50,
-                                              #command = open_bracket
+                                              fg_color = "#545454",
+                                              command = open_bracket
                                               )
 
 closing_bracket_button = customtkinter.CTkButton(master = window,
                                                  text=")",
                                                  width = 100,
                                                  height = 50,
-                                                 #command = close_bracket
+                                                 fg_color = "#545454",
+                                                 command = close_bracket
                                                  )
 
 del_button = customtkinter.CTkButton(master = window,
                                      text="Del",
                                      width = 100,
                                      height = 50,
-                                     #command = partial_delete
+                                     fg_color = "#545454",
+                                     command = partial_delete
                                      )
 
 #number buttons
@@ -186,70 +200,80 @@ number_0 = customtkinter.CTkButton(master = window,
                                    text="0",
                                    width = 100,
                                    height = 50,
-                                   #command = n_0
+                                   fg_color = "#eb711a",
+                                   command = n_0
                                    )
-
+#db6009
 number_1 = customtkinter.CTkButton(master = window,
                                    text="1",
                                    width = 100,
                                    height = 50,
-                                   #command = n_1
+                                   fg_color = "#eb711a",
+                                   command = n_1
                                    )
 
 number_2 = customtkinter.CTkButton(master = window,
                                    text="2",
                                    width = 100,
                                    height = 50,
-                                   #command = n_2
+                                   fg_color = "#eb711a",
+                                   command = n_2
                                    )
 
 number_3 = customtkinter.CTkButton(master = window,
                                    text="3",
                                    width = 100,
                                    height = 50,
-                                   #command = n_3
+                                   fg_color = "#eb711a",
+                                   command = n_3
                                    )
 
 number_4 = customtkinter.CTkButton(master = window,
                                    text="4",
                                    width = 100,
                                    height = 50,
-                                   #command = n_4
+                                   fg_color = "#eb711a",
+                                   command = n_4
                                    )
 
 number_5 = customtkinter.CTkButton(master = window,
                                    text="5",
                                    width = 100,
-                                   height = 50
-                                   #command = n_5
+                                   height = 50,
+                                   fg_color = "#eb711a",
+                                   command = n_5
                                    )
 
 number_6 = customtkinter.CTkButton(master = window,
                                    text="6",
                                    width = 100,
-                                   height = 50
-                                   #command = n_6
+                                   height = 50,
+                                   fg_color = "#eb711a",
+                                   command = n_6
                                    )
 
 number_7 = customtkinter.CTkButton(master = window,
                                    text = "7",
                                    width = 100,
                                    height = 50,
-                                   #command = n_7
+                                   fg_color = "#eb711a",
+                                   command = n_7
                                    )
 
 number_8 = customtkinter.CTkButton(master = window,
                                    text = "8",
                                    width = 100,
                                    height = 50,
-                                   #command = n_8
+                                   fg_color = "#eb711a",
+                                   command = n_8
                                    )
 
 number_9 = customtkinter.CTkButton(master = window,
                                    text = "9",
                                    width = 100,
                                    height = 50,
-                                   #command= n_9
+                                   fg_color = "#eb711a",
+                                   command= n_9
                                    )
 
 #define grid
@@ -258,11 +282,12 @@ window.columnconfigure((0,1,2,3), weight = 0)
 window.rowconfigure((0,1,2,3), weight = 0)
 
 #input field
-"""
-input_field.grid(row = 0, column = 0, columnspan = 4, padx = 10, pady = 10)
-output_frame.grid(row = 1, column = 0, columnspan = 4, padx = 10, pady = 5)
-output_field.grid(row=1, column = 0, padx = 10, columnspan = 4, pady = 5)
-"""
+
+input_field.grid(row = 0, column = 0, columnspan = 4, padx = (10,0), pady = (10,5), sticky = "nswe")
+
+output_frame.grid(row = 1, column = 0, columnspan = 4, padx = (10,0), pady = (5,10), sticky = "nwe")
+output_field.grid(row=1, column = 0, padx = 10, columnspan = 4, pady = 5, sticky = "nswe")
+
 # buttons
 
 number_7.grid(row=2, column=0, sticky = "w", padx = (10,0))
